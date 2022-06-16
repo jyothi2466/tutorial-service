03f92e3654ea7505fe2a0df01945aa5fb931d9f7
jyothi2466 <settij33@gmail.com>
2022-06-17 00:57:06 +0530

    adding source

--- src/main/java/com/hello/test/HelloServiceApplication.java
+++ src/main/java/com/hello/test/HelloServiceApplication.java
@@ -1,13 +1,13 @@
-package com.web.app;
+package com.hello.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class HelloServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(HelloServiceApplication.class, args);
 	}
 
 }
